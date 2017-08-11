f40a2eb1059fdbc523b1b0e77ae1013a664b1f0f
sutos88 <sutos88@gmail.com>
2017-08-11 14:03:15 +0200

    wip

--- sitemap.py
+++ sitemap.py
@@ -1,14 +1,16 @@
 import csv
 import re
 import sys
+import pandas as pd
 from datetime import datetime
 
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
+
+driver = webdriver.Chrome()
 inputLocation = "./Input/"
 outputLocation = "./Output/"
-driver = webdriver.Chrome()
 
 def ConvertDate():
 
@@ -20,35 +22,58 @@ def ConvertDate():
 
     return date
 
-def sitemap_crawler(homeUrl, actualUrl, depth, depthCount):
+outputFile = open(outputLocation + ConvertDate() + ".csv", encoding='utf-8', mode='w')
+outputFile.write("Actual Page Url,Previous page Url,Actual depth\n")
 
-    actualWebElements = driver.find_elements_by_tag_name("a")
 
-    if len(actualWebElements) == 0:
+def site_crawler(homeUrl, actualUrl, depth, depthCount):
+
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+
+    linkList = soup.find_all("a")
+
+    if len(linkList) == 0:
         return
 
-    soup = BeautifulSoup
+    for link in linkList:
+        linkurl = link.get('href')
+        if linkurl.startswith('/'):
+            outputFile.write(linkurl + "," + homeUrl + "," + str(depthCount+1) + "\n")
+            print(linkurl + "," + homeUrl + "," + str(depthCount+1) + "\n")
+            try:
+                driver.find_element_by_link_text(link.text).click()
+            except:
+                print("Unable to click: " + linkurl)
+                continue
+            driver.implicitly_wait(1)
+            if not depth>= depthCount+1:
+                site_crawler(homeUrl, linkurl, depth, depthCount+1 )
+
+
+
 
 
 def main(argv):
 
-    inputFile = open(inputLocation + "input.csv", encoding='utf-8', mode='r', newline='')
+
+    inputFile = open('C:\PycharmProjects\sitemap\Input\input.csv', encoding='utf-8', mode='r', newline='')
     inputReader = csv.DictReader(inputFile)
 
-    outputFile = open(outputLocation + ConvertDate() + ".csv", encoding='utf-8', mode='w')
-    outputFile.write("URL,User,Time\n")
+
 
     for index, row in enumerate(inputReader):
         url = row['Url']
-        depth = row['Depth']
+        depth = int(row['Depth'])
         depthCount = 1
 
         driver.get(url)
         driver.maximize_window()
 
-    sitemap_crawler(url, url, depth, depthCount)
-
-
+    site_crawler(url, url, depth, depthCount)
+    print("It's done.")
+    driver.close();
+    inputFile.close()                                                                       # Close the input and output file
+    outputFile.close()
 
 if __name__ == "__main__":
     main(sys.argv)
